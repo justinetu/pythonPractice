a5272bdac3a93a301273ffd10daf190cf31b26e0
Justin Etu <71947079+justinetu@users.noreply.github.com>
2022-11-19 19:55:41 -0600

    Create for_loops.py

---
+++ for_loops.py
@@ -0,0 +1,6 @@
+# Simple python program that calculates the total of a given list of values
+prices = [2.00, 2.50, 20.0]
+total = 0
+for price in prices:
+    total += price
+print(f"Your total cost is: $ {total}")
